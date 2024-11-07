265286a263218e3038d7dffd5cdf99a3b9856cf7
darkhorse1081 <186711485+darkhorse1081@users.noreply.github.com>
2024-11-07 21:43:57 +1300

    comment update

--- visual.cryptography/proj/CreatePatterns.m
+++ visual.cryptography/proj/CreatePatterns.m
@@ -1,12 +1,15 @@
 function[pattern_cells] = CreatePatterns()
+    % This function creates a cell array containing six different 2x2 patterns.
+    % Each pattern is represented as a 2x2 matrix with values of 0 or 255 (black or white).
 
-pattern_cells = cell(1,6);
-
-pattern_cells{1} = uint8([255,255;0,0]);
-pattern_cells{2} = uint8([0,0;255,255]);
-pattern_cells{3} = uint8([0,255;0,255]);
-pattern_cells{4} = uint8([255,0;255,0]);
-pattern_cells{5} = uint8([0,255;255,0]);
-pattern_cells{6} = uint8([255,0;0,255]);
+    pattern_cells = cell(1,6);
+    
+    % Define each pattern as a 2x2 uint8 matrix and assign it to the cell array.
+    pattern_cells{1} = uint8([255,255;0,0]);
+    pattern_cells{2} = uint8([0,0;255,255]);
+    pattern_cells{3} = uint8([0,255;0,255]);
+    pattern_cells{4} = uint8([255,0;255,0]);
+    pattern_cells{5} = uint8([0,255;255,0]);
+    pattern_cells{6} = uint8([255,0;0,255]);
 
 end
--- visual.cryptography/proj/GenerateKey.m
+++ visual.cryptography/proj/GenerateKey.m
@@ -1,12 +1,16 @@
 function[output_key] = GenerateKey(integer_values, pattern_create)
 
-[row,col] = size(integer_values);
-output_key = cell(size(integer_values));
+    [row,col] = size(integer_values);
 
-for i = 1:row
-    for j = 1:col
-        output_key{i,j} = pattern_create{integer_values(i,j)};
+    % Initialize output_key as a cell array with the same size as integer_values.
+    output_key = cell(size(integer_values));
+    
+    for i = 1:row
+        for j = 1:col
+            % Use the integer value at position (i, j) to select a pattern
+            % from pattern_create and assign it to the corresponding cell in output_key.
+            output_key{i,j} = pattern_create{integer_values(i,j)};
+        end
     end
-end
 
 end
