30f1f3bdccce705235e163bd42e34780eeda2ebf
darkhorse1081 <186711485+darkhorse1081@users.noreply.github.com>
2024-11-09 05:27:06 +1300

    final update with comments

--- visual.cryptography/proj/ImageToPatterns.m
+++ visual.cryptography/proj/ImageToPatterns.m
@@ -4,14 +4,19 @@ function[cellPattern] = ImageToPatterns(greyscaleImage)
 [row,col] = size(greyscaleImage);
 cellPattern = cell(row/2,col/2);
 
+% counter updates upon iteration through greyscale dimensions
 count = 0;
 second_count = 0;
 
+% loop iterated every two spaces based on 2x2 factor for each block
 for i = 1:2:row
     second_count = second_count + 1;
     for j = 1:2:col
             count = count + 1;
             storageArray = uint8(zeros(2,2));
+
+            % storage interior array concatanated with greyscale values
+            % before added to cell
             storageArray = storageArray + greyscaleImage(i:i+1,j:j+1);
             cellPattern{i-(second_count-1),j-(count-1)} = storageArray;
             if j == col-1
