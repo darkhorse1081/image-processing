95bb0c1132059c6b869d390b3b1b993af214ab7b
darkhorse1081 <186711485+darkhorse1081@users.noreply.github.com>
2024-11-09 04:22:08 +1300

    logic change

--- visual.cryptography/proj/ImageToPatterns.m
+++ visual.cryptography/proj/ImageToPatterns.m
@@ -4,27 +4,31 @@ function[cellPattern] = ImageToPatterns(greyscaleImage)
 [row,col] = size(greyscaleImage);
 cellPattern = cell(row/2,col/2);
 
-storageArray = uint8(zeros(2,2));
-
 count = 0;
 second_count = 0;
 
 for i = 1:2:row
     second_count = second_count + 1;
-    for j = 1:2:col
-            count = count + 1;
-            for m = i:i+1
-                for n = j:j+1
-                    storageArray(m,n) = greyscaleImage(i,j);
+    if col>1
+        for j = 1:2:col
+                count = count + 1;
+                storageArray = uint8(zeros(2,2));
+                storageArray = storageArray + greyscaleImage(i:i+1,j:j+1);
+                cellPattern{i-(second_count-1),j-(count-1)} = storageArray;
+                if j == col
+                    count = 0;
                 end
-            end
-            cellPattern{i-(second_count-1),j-(count-1)} = storageArray;
-            if j == col
-                count = 0;
-            end
+        end
+    else
+        count = count + 1;
+        storageArray = uint8(zeros(2,2));
+        storageArray = storageArray + greyscaleImage(i:i+1,j:j+1);
+        cellPattern{i-(second_count-1),j-(count-1)} = storageArray;
+        if j == col
+            count = 0;
+        end
+
     end
 end
-
-
 
 end
