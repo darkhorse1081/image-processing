c368613115b7589e0ed554479e03380556d7eb7c
darkhorse1081 <186711485+darkhorse1081@users.noreply.github.com>
2024-12-11 14:55:58 +1300

    change folders

--- visual.cryptography/proj/test/cryptography.m
+++ visual.cryptography/proj/test/cryptography.m
@@ -147,3 +147,17 @@ imwrite(plainImage,'plainImage.png');
 figure(3)
 imshow(plainImage);
 title('Decrypted Image');
+
+%% AlterByOne
+function [final] = AlterByOne(value)
+    % This function adjusts the input value by adding or subtracting 1.
+    
+    if value == 255
+        % If the input value is 255, subtract 1 to avoid overflow.
+        final = uint8(value - 1);
+    elseif value < 255
+        % If the input value is less than 255, add 1 to increment it.
+        final = uint8(value + 1);
+    end
+end
+
